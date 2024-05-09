e81fd1c1ddb9a46437df1d88744c0a90b0ae9c7d
putra2016 <skputra2016@gmail.com>
2024-05-09 20:47:37 +0700

    Update database.php

--- Support-Ticket/source/config/database.php
+++ Support-Ticket/source/config/database.php
@@ -66,7 +66,7 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'localhost2'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
