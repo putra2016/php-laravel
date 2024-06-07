84a825ed318036ad263d3ba051cf647d0799a8ea
putra2016 <skputra2016@gmail.com>
2024-06-07 15:47:43 +0700

    Update database.php

--- Support-Ticket/source/config/database.php
+++ Support-Ticket/source/config/database.php
@@ -81,7 +81,7 @@ return [
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'localhost2'),
+            'host' => env('DB_HOST', 'localhost1'),
             'port' => env('DB_PORT', '1433'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
