a1229717bb90f76fc10dab9eb0e9332376b372cd
putra-lab <skputra2016@gmail.com>
2024-07-07 20:01:07 +0700

    Update database.php

--- Support-Ticket/source/config/database.php
+++ Support-Ticket/source/config/database.php
@@ -139,7 +139,7 @@ return [
 
         'cache' => [
             'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'host' => env('REDIS_HOST', '127.0.0.11'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
